ac7b740544bace2eb5fb0642056cd473b406d515
enestrz <enestrz15@gmail.com>
2020-03-16 01:47:09 +0300

    Added Visualize using JS and CSS

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Section: Web Development 101
 Project: Rock Paper Scissors
 
 This project based on JavaScript. So open developer tool on your browser and click console tab.
+Added visualize now!
--- main.js
+++ main.js
@@ -1,12 +1,39 @@
+//Construct DOM with JS
+const body = document.querySelector('body');
+
+
+//Welcome Screen Elements
+const welcomeButton = document.querySelector('#welcomeButton');
+
+//Game Screen Elements
+const buttons = document.querySelectorAll('.buttons');
+const div = document.createElement('div');
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+div.appendChild(p1);
+div.appendChild(p2);
+div.setAttribute('id','contes');
+p2.setAttribute('id','score');
+body.appendChild(div);
+
+//End Game Screen Elements
+const againButton = document.querySelector('#againButton');
+againButton.style.display = "none";
+div.appendChild(againButton);
+
+//Functions
 var computerScore = 0;
 var playerScore = 0;
+var round;
+
+
 function computerPlay(){
     var choice = ["Rock","Paper","Scissors"];
     return choice[Math.floor(Math.random() * choice.length)]
 }
-
-function playRound(playerSelection,computerSelection){
-     
+ 
+function playRound(playerSelection){
+    let computerSelection = computerPlay();
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors"){
         playerScore++ ;
         return "You win! Rock beats Scissors";
@@ -39,32 +66,58 @@ function playRound(playerSelection,computerSelection){
 
 function game(){
     
-    for(var i = 0 ; i<5 ; i++){
-        var computerSelection = computerPlay();
-        var playerSelection = prompt("Enter Rock, Paper or Scissors please:");
-        if(playerSelection.toLowerCase()!= "rock" && playerSelection.toLowerCase()!= "paper" && playerSelection.toLowerCase()!= "scissors"){
-            alert("You typed wrong!");
-            i--;
-            continue;   
-        }
-        else{
-        let winner = playRound(playerSelection,computerSelection)
-        alert(winner + " Score: Player= "+playerScore+" vs Computer= "+computerScore);
-        console.log(winner + " Score: Player= "+playerScore+" vs Computer= "+computerScore);  
+    //div.innerHTML = "Player: "+playerScore+" vs Computer: "+computerScore +"<br />" + round;
+    p1.innerHTML = "Player "+playerScore+" - "+ computerScore+" Computer";
+    p2.innerHTML = round;
+    if (playerScore==5 ||computerScore ==5 ){
+       // div.innerHTML = "Player: "+playerScore+" vs Computer: "+computerScore +"<br />" + round; 
+       
+        setTimeout(function() {
+            if(playerScore ==5){
+                p1.innerHTML= "You win!";
+                
+            }
+            else{
+                p1.innerHTML= "You lose!!";
+                
+            }
+            p2.innerHTML = "Player "+playerScore+" - "+ computerScore+" Computer";
+            document.querySelectorAll('.buttons')[0].style.display = "none";
+            document.querySelectorAll('.buttons')[1].style.display = "none";
+            document.querySelectorAll('.buttons')[2].style.display = "none";
+            againButton.style.display = "inline-block";
+        },10)
+       
+        
     }
+   
+} 
+welcomeButton.addEventListener('click', (e) => {
+    buttons.forEach((button) => {
+        welcomeButton.style.display = "none";
+        button.style.display = "inline-block";
+
     }
-    if (playerScore>computerScore){
-        alert("The winner is Player!");
-    }
-    else if(playerScore<computerScore){
-        alert("You lose!");
-    }
-    else{
-        alert("It's a draw!");
-    }
-}
+    );
+
+});
 
 
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+       round = playRound(button.value);
+       game();
+      });
+});
 
-game();
+againButton.addEventListener('click',(e) => {
+    computerScore = 0;
+    playerScore = 0;
+    againButton.style.display = "none";
+    p1.innerHTML = "Player "+playerScore+" - "+ computerScore+" Computer";
+    p2.innerHTML = "";
+    document.querySelectorAll('.buttons')[0].style.display = "inline-block";
+    document.querySelectorAll('.buttons')[1].style.display = "inline-block";
+    document.querySelectorAll('.buttons')[2].style.display = "inline-block";
 
+});
